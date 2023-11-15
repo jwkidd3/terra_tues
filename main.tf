0eea7981dd160e2d8c404469416d8993e092f0d9
jwkidd3 <jkidd@kiddcorp.com>
2023-11-14 21:19:23 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,9 +29,9 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   count=3
-
+  key_name="user25_deployer-key"
   tags = {
-    Name = "instance-${count.index}",
+    Name = "user25-${count.index}",
     role=count.index==0?"lb": (count.index<3?"web":"backend")
   }
 }
